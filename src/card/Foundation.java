97521a6457c53d2a9fa68fc703ce354915b0efa3
Jackson Whitfield <whitfieldjackson38@gmail.com>
2019-03-29 19:33:04 -0400

    Update Foundation.java

--- src/card/Foundation.java
+++ src/card/Foundation.java
@@ -1,27 +1,11 @@
 package card;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-
 /**
  * A foundation is a {@link StackOfCards} in which all cards must be of the same
  * suit and each card's value is 1 more than that of the card below it. The bottom
  * card must be an ace.
  * @author Warren Godone-Maresca
  */
-public class Foundation extends StackOfCards 
-{
-	/** int for keeping track of value for year for Anno Domini game*/
-	private int digit;
-	
-	/** char array that stores digits of the current year*/
-	String yearNum = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
-	char[] yearArray = yearNum.toCharArray();
-	
-	/** Array that stores all suites*/
-	public static List<String> suitsUsed = new ArrayList();
-
+public class Foundation extends StackOfCards {
 	/**
 	 * Instantiates an empty <code>Foundation</code> where all cards will have
 	 * no size and positioned at the origin.
@@ -40,9 +24,8 @@ public class Foundation extends StackOfCards
 	 * 					bottom of the stack.
 	 * @param cardWidth	The width of each card in the stack.
 	 */
-	public Foundation(int x, int y, int cardWidth, int digit){
+	public Foundation(int x, int y, int cardWidth){
 		super(x, y, cardWidth, 0, 0);
-		this.digit = digit;
 	}
 
 	/**
@@ -54,93 +37,46 @@ public class Foundation extends StackOfCards
 	 * 			above conditions.
 	 */
 	@Override
-	public void push(Card card)
-	{
-		if(isEmpty())
-		{                //If this is empty,
-			if(card.getValue() == 1)
-			{ //then the card must be an ace to be pushed.
+	public void push(Card card){
+		if(isEmpty()){                //If this is empty,
+			if(card.getValue() == 1){ //then the card must be an ace to be pushed.
 				super.push(card);
-			} 
-			else 
-			{
+			} else {
 				throw new IllegalArgumentException();//TODO ""
 			}
-		} 
-		else 
-		{
+		} else {
 			//Otherwise the card's value must be 1 greater than the top card
 			//and be of the same suit.
 			if(card.getValue() == peek().getValue() + 1 
-					&& card.getSuit() == peek().getSuit())
-			{
+					&& card.getSuit() == peek().getSuit()){
 				super.push(card);
-			} 
-			else 
-			{
+			} else {
 				throw new IllegalArgumentException();
 			}
 		}
 	}	
 	
-	/**
-	 * Code to make the foundations start only accepting first cards based on the current year
-	 * Also lets foundation stacking loop around
-	 * Specialized for Anno Domini game
-	 * @throws IllegalArgumentException if <code>card</code> does not meet the
-	 * 			above conditions.
-	 */
-	public void annoPush(Card card)
-	{
-		boolean suitCanAdd = true;
-		if(isEmpty())
-		{                //If this is empty, check to see if the card values align with the year digits
-			for (int i = 0; i < suitsUsed.size(); i++)
-			{
-				if (suitsUsed.get(i).equals(card.getSuit().name()))
-				{
-					suitCanAdd = false;
-				}
-			}
-			
-			if(
-				(
-					card.getValue() == (Character.getNumericValue(yearArray[digit]) + 1) 
-					&& suitCanAdd == true
-				)
-				||
-				(
-					card.getValue() == 11 
-					&& Character.getNumericValue(yearArray[digit]) == 0
-					&& suitCanAdd == true
-				)
-			  )
-			{ //then the card must correspond to the digit of the year to be placed
-				suitsUsed.add(card.getSuit().name());
+	public void push2(Card card, Card base) {
+		if(isEmpty()){                //If this is empty,
+			if(card.getValue() == base.getValue()){ //then the card must be an ace to be pushed.
 				super.push(card);
-			} 
-			else 
-			{
+			} else {
 				throw new IllegalArgumentException();//TODO ""
 			}
-		} 
-		else 
-		{
+		} else {
 			//Otherwise the card's value must be 1 greater than the top card
 			//and be of the same suit.
-			//OR it can be an ace being placed on a king.
-			if((card.getValue() == peek().getValue() + 1 
-					&& card.getSuit() == peek().getSuit())
-				|| (card.getValue() == 1 
-					&& peek().getValue() ==  13
-					&& card.getSuit() == peek().getSuit()))
-			{
+			if(card.getValue() == peek().getValue() + 1 
+					&& card.getSuit() == peek().getSuit()){
 				super.push(card);
-			} 
-			else 
-			{
+			} else {
 				throw new IllegalArgumentException();
 			}
 		}
-	}	
+	}
+	
+	
+	public void pushBase(Card card) {
+		super.push(card);
+	}
 }
